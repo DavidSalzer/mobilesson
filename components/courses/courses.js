84d0435eb72e0e12098d9c67ff90fac31cc6fe3f
shaigiv <gshai@cambium.co.il>
2014-12-10 08:46:39 +0200

    - courses charts

--- components/courses/courses.js
+++ components/courses/courses.js
@@ -11,5 +11,36 @@ mobilessonApp.controller('courses', ['$rootScope', '$scope', '$state', '$http', 
         console.log(data);
     })
 
+     //Morris Chart
+    var donutChart = Morris.Donut({
+        element: 'donutChartCourses',
+        data: [
+		{ label: "Finished", value: 100 },
+		{ label: "In Progress", value: 3091 },
+		{ label: "Haven't Started", value: 2781 }
+	  ],
+        colors: ['#f3ce85', '#65CEA7', '#FC8675']
+    });
+
+    var barChart = Morris.Bar({
+        element: 'barChartCourses',
+        data: [
+		{ y: '2006', a: 150, b: 90 },
+		{ y: '2007', a: 75, b: 65 },
+		{ y: '2008', a: 50, b: 40 },
+		{ y: '2009', a: 75, b: 65 },
+		{ y: '2010', a: 50, b: 40 },
+		{ y: '2011', a: 75, b: 65 },
+		{ y: '2012', a: 100, b: 90 }
+	  ],
+        xkey: 'y',
+        ykeys: ['a', 'b'],
+        grid: false,
+        labels: ['Installed', 'Finished'],
+        barColors: ['#5EE1B1', '#3BC894'],
+        gridTextColor: '#fff'
+    });
+
+
 } ])
 
