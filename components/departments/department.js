6faa0c18b068aaf3999b2966a143c5e033aa711e
shaigiv <gshai@cambium.co.il>
2014-12-23 17:08:27 +0200

    - department.html modal

--- components/departments/department.js
+++ components/departments/department.js
@@ -1,14 +1,38 @@
 mobilessonApp.controller('department', ['$rootScope', '$scope', '$state', '$stateParams', '$http', function ($rootScope, $scope, $state, $stateParams, $http) {
     $('#scroll-to-top').click();
 
-    $http.get(domain + 'statistics/MobiStatistics.php?action=getStatisticsByUnit&unit=department&id=' + $stateParams.departmentId)
+    //$scope.departmentId = $stateParams.departmentId;
+    $http.get(domain + 'mobiEngine3.4_DEV/statisticsServer.php?action=getStatisticsByUnit&unit=department&id=' + $stateParams.departmentId)
     .success(function (data) {
         if (data.status == 'ok') {
             $scope.statistics = data.data;
+
+            $scope.courses = data.data.statisticsByCourse;
+            for (i = 0; i < $scope.courses.length; i++) {
+                $scope.courses[i].completedPercent = $scope.courses[i].employees * $scope.courses[i].ended / 100;
+            }
+            console.log($scope.statistics)
         }
     })
     .error(function (data) {
         console.log(data);
     })
 
+    $scope.getEmployeesByCourse = function () {
+        $http.get(domain + 'mobiEngine3.4_DEV/statisticsServer.php?action=getStatisticsByUnit&unit=department&id=' + $stateParams.departmentId)
+    .success(function (data) {
+        if (data.status == 'ok') {
+            $scope.statistics = data.data;
+
+            $scope.courses = data.data.statisticsByCourse;
+            for (i = 0; i < $scope.courses.length; i++) {
+                $scope.courses[i].completedPercent = $scope.courses[i].employees * $scope.courses[i].ended / 100;
+            }
+            console.log($scope.statistics)
+        }
+    })
+    .error(function (data) {
+        console.log(data);
+    })
+    }
 } ])
--- components/sidebar/sidebar.js
+++ components/sidebar/sidebar.js
@@ -1,12 +1,27 @@
-mobilessonApp.controller('sidebar', ['$rootScope', '$scope', '$state', '$stateParams', '$http', function ($rootScope, $scope, $state, $stateParams, $http) {
+mobilessonApp.controller('sidebar', ['$rootScope', '$scope', '$state', '$stateParams', '$http', '$timeout', function ($rootScope, $scope, $state, $stateParams, $http, $timeout) {
+    $scope.departments = []; // [{ 'name': 'transport', 'id': '123' }, { 'name': 'finance', 'id': '456'}];
     $scope.currentPage = 'courses';
-    $scope.navigate = function (page,params) {
+    $scope.navigate = function (page, params) {
         console.log(page);
-        $state.transitionTo(page,params)
+        $state.transitionTo(page, params)
         //add class active
         //remove active from others
         //transition to
         //if openable- slide down
         // if page=currentpage
     }
+
+    $http.get(domain + 'mobiEngine3.4_DEV/statisticsServer.php?action=getDepartments')//&offset=&limit=&status
+    .success(function (data) {
+        if (data.status == 'ok') {
+            $timeout(function () {
+                $scope.departments = data.data;
+            }, 0);
+            console.log(data.data);
+        }
+    })
+    .error(function (data) {
+        console.log(data);
+    })
+
 } ])
